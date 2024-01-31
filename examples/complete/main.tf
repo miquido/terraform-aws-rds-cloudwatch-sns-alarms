2dce50001f57b0dbbe68202f2359ce6d54f2c05a
Marek Mościchowski <marek.moscichowski@miquido.com>
2024-01-31 14:38:54 +0100

    fix: tf validate

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,8 +6,6 @@ variable "region" {
 provider "aws" {
   region = var.region
 
-  # Make it faster by skipping something
-  skip_get_ec2_platforms      = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
@@ -21,7 +19,6 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   identifier_prefix    = "rds-server-example"
-  name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
